ed5e48b46c795d3d66ae004d15f1de8d263a8c07
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-08-02 23:50:04 +0700

    add: ersiapan Pelajari Multiple Page Di Project Vue

---
+++ learn-VueRouter/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Home />
+  <About />
+</template>
+
+<script setup>
+import Home from "./views/Home.vue";
+import About from "./views/About.vue";
+</script>
+
+<style lang="scss" scoped>
+
+</style>
