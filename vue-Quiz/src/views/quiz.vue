a1e2d91cb771dc748a6e3d7815f1a8e262dcc94e
yofandi riki winata <yofandirikiwinata24@gmail.com>
2024-08-09 23:30:31 +0700

    add: Mendapatkan Nilai Dari Perubahan State Dengan Computed

--- vue-Quiz/src/views/quiz.vue
+++ vue-Quiz/src/views/quiz.vue
@@ -12,7 +12,7 @@
 <script setup>
 import quizHeader from "@/components/quizHeader.vue";
 import quizContent from "@/components/quizContent.vue";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRoute } from "vue-router";
 import quizes from "../data/quizes.json";
 
@@ -21,18 +21,22 @@ const QuizId = parseInt(route.params.id);
 const quiz = quizes.find((q) => q.id === QuizId);
 
 const currentQuestionIndex = ref(0);
-const questionPage = ref(
-  `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
-);
+const questionPage = computed(() => {
+  return `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`;
+});
 
-watch(
-  () => currentQuestionIndex.value,
-  () => {
-    questionPage.value = `${currentQuestionIndex.value + 1} / ${
-      quiz.questions.length
-    }`;
-  }
-);
+// const questionPage = ref(
+//   `${currentQuestionIndex.value + 1} / ${quiz.questions.length}`
+// );
+
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionPage.value = `${currentQuestionIndex.value + 1} / ${
+//       quiz.questions.length
+//     }`;
+//   }
+// );
 </script>
 
 <style scoped></style>
